2ced85bcd6eeb608dc7001e6bf8d135d00c72090
milayamily <97246447+milayamily@users.noreply.github.com>
2022-03-16 20:33:01 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,15 +4,15 @@
 int main()
 {
   FILE *f1, *f2;
-  char arr[1001][1001], s, u;
+  char arr[1001][1001], s;
   int i = 0, j = 0;
-  f1 = fopen("matrix_of_incendence18.txt", "r"); // открываем файл
+  f1 = fopen("matrix_of_incendence18.txt", "r"); // открываю файл с матрицей
   while (!feof(f1))
   {
     fscanf(f1, "%c", &s);
-    if (s != ' ' && s != '\n')
+    if (s != ' ' && s != '\n') // обрабатываю входящий файл с матрицей и записываю ее в массив
     {
-      arr[i][j] = s; // записываем матрицу в массив
+      arr[i][j] = s;
       j++;
       if (j == 20)
       {
@@ -20,9 +20,11 @@ int main()
         j = 0;
       }
     }
-  f2 = fopen("graf.gv", "w"); // создаем файл для Graphviz
-  fprintf(f2, "graph grath {\n"); // Первая строка, чтобы Graphviz октрыл его правильно
-  for (int a = 0; a < 20; a++) // записываем список смежности в gv файл
+  }
+  char u;
+  f2 = fopen("graf.gv", "w"); // создаю файл graf.gv и записываю туда список пар вершин соединенных ребрами
+  fprintf(f2, "graph grath {\n");
+  for (int a = 0; a < 20; a++)
   {
     u = -1;
     for (int y = 0; y < 5; y++)
@@ -42,10 +44,9 @@ int main()
         }
       }
     }
-    fprintf(f2, ";\n");
-    printf(";\n");
+    fprintf(f2, ";\n"); // записываю значения в файл
+    printf(";\n"); // отображаю значения в терминале
   }
   fprintf(f2, "}");
-  fclose(f2); // закрываем и сохраняем файл
-}
+  fclose(f2); // закрываю файл
 }
